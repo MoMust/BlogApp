c51046ec7efd93bc456fb3fe3a8509802c34c6c4
Mohammd Mustafa <68998715+MoMust@users.noreply.github.com>
2023-09-13 16:07:58 +0200

    First commit, Creating responsivness

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,34 @@
 import styles from "./homepage.module.css";
+import Link from "next/link"
+
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <Link href="/">
+      <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste voluptates dolorum laboriosam molestiae? Minima assumenda optio corrupti nobis, eveniet recusandae doloremque ab itaque, voluptates cum praesentium eligendi sit obcaecati maxime.</h1>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id dolore esse
+      asperiores! Non cumque, neque tempora commodi eveniet labore! Minima alias
+      repellat illo facilis, assumenda voluptate! Doloribus debitis porro
+      sapiente.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id
+      dolore esse asperiores! Non cumque, neque tempora commodi eveniet labore!
+      Minima alias repellat illo facilis, assumenda voluptate! Doloribus debitis
+      porro sapiente.Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Id dolore esse asperiores! Non cumque, neque tempora commodi eveniet
+      labore! Minima alias repellat illo facilis, assumenda voluptate! Doloribus
+      debitis porro sapiente. Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit. Id dolore esse asperiores! Non cumque, neque tempora
+      commodi eveniet labore! Minima alias repellat illo facilis, assumenda
+      voluptate! Doloribus debitis porro sapiente. Lorem ipsum dolor sit amet,
+      consectetur adipisicing elit. Id dolore esse asperiores! Non cumque, neque
+      tempora commodi eveniet labore! Minima alias repellat illo facilis,
+      assumenda voluptate! Doloribus debitis porro sapiente.Lorem ipsum dolor
+      sit amet, consectetur adipisicing elit. Id dolore esse asperiores! Non
+      cumque, neque tempora commodi eveniet labore! Minima alias repellat illo
+      facilis, assumenda voluptate! Doloribus debitis porro sapiente. Lorem
+      ipsum dolor sit amet, consectetur adipisicing elit. Id dolore esse
+      asperiores! Non cumque, neque tempora commodi eveniet labore! Minima alias
+      repellat illo facilis, assumenda voluptate! Doloribus debitis porro
+      sapiente.
+    </Link>
+  );
 }
