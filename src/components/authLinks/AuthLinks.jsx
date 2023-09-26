aff5c964c309b40f2173a818b82de98f37f9f21f
Mohammd Mustafa <68998715+MoMust@users.noreply.github.com>
2023-09-26 15:39:47 +0200

    responsivness links for tablet and mobile, switch dark & light mode

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+"use client"
+import React, {useState} from "react";
 import style from "./authLinks.module.css";
 import Link from 'next/link';
-const AuthLinks = () => {
 
+
+const AuthLinks = () => {
+  const [open, setOpen] = useState(false)
   // Temporary
-  const status = "authenticated"
+  const status = "notauthenticated"
   return (
     <>
       {status === "notauthenticated" ? (
-        <Link href="/login">Login</Link>
+        <Link href="/login" className={style.link}>
+          Login
+        </Link>
       ) : (
         <>
-        <Link href="/write">Write</Link>
-        <span className={style.link}>Logout</span>
+          <Link href="/write" className={style.link}>
+            Write
+          </Link>
+          <span className={style.link}>Logout</span>
         </>
+      )}
+      <div className={style.burger} onClick={() => setOpen(!open)}>
+        <div className={style.line}></div>
+        <div className={style.line}></div>
+        <div className={style.line}></div>
+      </div>
+      {open && (
+        <div className={style.responsiveMenu}>
+          <Link href="/" className="link">
+            Homepage
+          </Link>
+          <Link href="/" className="link">
+            About
+          </Link>
+          <Link href="/" className="link">
+            Contact
+          </Link>
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={style.link}>Logout</span>
+            </>
+          )}
+        </div>
       )}
     </>
   );
