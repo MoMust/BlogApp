aef7dd95986b007a5f85c020e291a8a408a9c919
Mohammed Mustafa <68998715+MoMust@users.noreply.github.com>
2023-11-14 15:23:23 +0100

    setup mongoDb, setup prisma schema config adapter, implement verify authentication, add prisma package, get categories using prisma studio (#12)

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -3,51 +3,45 @@ import Styles from "./categoryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const CategoryList = () => {
+
+const getData = async () => {
+  const resp = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+
+  if (!resp.ok) {
+    throw new Error("Failed");
+  }
+
+  return resp.json();
+};
+
+const CategoryList = async () => {
+  const data = await getData();
+
+  console.log("data:",data)
+
   return (
     <div className={Styles.container}>
-      <h1 className={Styles.title}>Popular titles</h1>
+      <h1 className={Styles.title}>Popular Categories</h1>
       <div className={Styles.categories}>
-          <Link href="/blog?cat=style" className={`${Styles.category} ${Styles.style}`}>
-            <Image
-              src="/style.png"
-              alt="style"
-              width={32}
-              height={32}
-              className={Styles.image}
-            />
-            Style
-          </Link>
-          <Link href="/blog" className={`${Styles.category} ${Styles.travel}`}>
-            <Image
-              src="/travel.png"
-              alt="travel"
-              width={32}
-              height={32}
-              className={Styles.image}
-            />
-            travel
-          </Link>
-          <Link href="/blog" className={`${Styles.category} ${Styles.culture}`}>
-            <Image
-              src="/culture.png"
-              alt="culture"
-              width={32}
-              height={32}
-              className={Styles.image}
-            />
-            culture
-          </Link>
-          <Link href="/blog" className={`${Styles.category} ${Styles.coding}`}>
-            <Image
-              src="/coding.png"
-              alt="coding"
-              width={32}
-              height={32}
-              className={Styles.image}
-            />
-            coding
-          </Link>
+          {data.categories?.map((item) =>(
+            <Link
+              href="/blog?cat=style"
+              className={`${Styles.category} ${Styles[item.slug]}`}
+              key={item._id}
+            >
+             { item.img && (<Image
+                src={item.img}
+                alt="style"
+                width={32}
+                height={32}
+                className={Styles.image}
+              />)}
+              {item.title}
+            </Link>
+          ))}
+          
       </div>
     </div>
   );
