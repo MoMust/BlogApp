db4c3a17d5af1d5487f111d09d8f380bce6180b2
Mohammed Mustafa <68998715+MoMust@users.noreply.github.com>
2023-11-19 16:19:20 +0100

    Get data of cat and posts api, set up route for posts and cat with prisma and mogodb, pagination, (#13)

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,21 +2,22 @@ import Image from "next/image";
 import Styles from "./card.module.css";
 import Link from "next/link";
 
-const Card = () => {
+const Card = ({data, key}) => {
+
+  
   return (
-    <div className={Styles.container}>
+    <div className={Styles.container} key={key}>
       <div className={Styles.imageContainer}>
         <Image src="/p1.jpeg" alt="" fill className={Styles.image} />
       </div>
       <div className={Styles.textContainer}>
         <div className={Styles.detail}>
-          <span className={Styles.date}>01.07.2023 - </span>
-          <span className={Styles.category}>Culture</span>
+          <span className={Styles.date}>{data.createdAt}</span>
+          <span className={Styles.category}>{data.catSlug}</span>
         </div>
         <Link href="/">
           <h1>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero,
-            adipisci?
+            {data.title}
           </h1>
         </Link>
         <p className={Styles.desc}>
